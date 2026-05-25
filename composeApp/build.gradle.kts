f946003488659aad0326340e753bb53e8f85e4f0
LilShineGang <blagovestdoukovy2@gmail.com>
2026-05-25 15:32:06 +0200

    merge: sync danfront with origin/main

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -63,6 +63,8 @@ kotlin {
 
             // Navigation
             implementation(libs.navigation.compose)
+            // Icons
+            implementation("org.jetbrains.compose.material:material-icons-extended:1.7.3")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
